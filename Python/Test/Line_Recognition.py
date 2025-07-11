f749c0b8d9ba41aa22bc730d25eb0565ae580539
Shining101 <caidensquizz@gmail.com>
2025-07-11 01:55:51 -0400

    adjusted layout

--- Python/Test/Line_Recognition.py
+++
@@ -1,5 +0,0 @@
-epsilon = 2
-
-#formula for x gradient: 1/(2*epsilon) * ((image[i+1][j+1] - image[i][j+1]) + (image[i+1][j] - image[i][j]))
-
-#formula for y gradient: 1/(2*epsilon) * ((image[i+1][j+1] - image[i+1][j]) + (image[i][j+1] - image[i][j]))
